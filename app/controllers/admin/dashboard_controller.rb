5be0e39461676aed1b0efd084afd940ad5a7885f
Justin Lam <justin.kh.lam@gmail.com>
2022-01-16 23:30:01 +0000

    add basic HTML authentication to admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,6 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: ENV['ADMIN_USER'], password: ENV['ADMIN_KEY']
+
   def show
     @product_count = LineItem.count
     @category_count = Category.count
