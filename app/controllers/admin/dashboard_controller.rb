ea153eac30e8ee57064a20d135e3f0b58ebace44
Justin Lam <justin.kh.lam@gmail.com>
2022-01-16 23:28:01 +0000

    implement search queries for product/category count, add table in views

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,9 @@
 class Admin::DashboardController < ApplicationController
   def show
+    @product_count = LineItem.count
+    @category_count = Category.count
+
+    # If I want to pull other info
+    # @category_names = Category.all
   end
 end
