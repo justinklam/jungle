7d4df5dedc86e974e882b72cae9388a397a9986f
Justin Lam <justin.kh.lam@gmail.com>
2022-01-18 22:19:21 +0000

    add in additional logic for controller methods. add in base html in index/new.html.erb. add in top/nav for admin:categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,4 +1,5 @@
 class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with name: ENV['ADMIN_USER'], password: ENV['ADMIN_KEY']
 
   # show all current categories
   def index
@@ -8,12 +9,26 @@ class Admin::CategoriesController < ApplicationController
 
   # render new form
   def new
-    render :new
+    @new_categories = Category.new
   end
 
   # database insert
   def create
-    @new_categories = Category.new
+    @new_categories = Category.new(category_params)
+    
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:product).permit(
+      :name
+    )
   end
 
 end
