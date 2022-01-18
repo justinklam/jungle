813097e1addefef25088589634dfe95680ed35a4
Justin Lam <justin.kh.lam@gmail.com>
2022-01-18 04:30:57 +0000

    add in base logic to categories_controller

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -2,14 +2,18 @@ class Admin::CategoriesController < ApplicationController
 
   # show all current categories
   def index
+    @categories = Category.all
+    render :index
   end
 
   # render new form
   def new
+    render :new
   end
 
   # database insert
   def create
+    @new_categories = Category.new
   end
 
 end
