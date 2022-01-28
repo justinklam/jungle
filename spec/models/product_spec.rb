1d9eb1151bf38d82187d47731a64d4ab8dd18dd8
Justin Lam <justin.kh.lam@gmail.com>
2022-01-28 22:27:40 +0000

    initialize product_spec.rb testing and add in logic for product_validation

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+
+  describe 'Validations' do
+
+    context "Insert product" do
+      it "should create a valid product" do
+        # ! return true/false if exist
+        cat1 = Category.create! name: 'book'
+        cat1.products.create!({ name: 'coding_book', price: 7, quantity: 3}).valid?
+      end
+
+      # This should fail
+      # cat1 = Category.create! name: 'book'
+      # product1 = cat1.products.create!({ name: nil, price: 7, quantity: 3})
+      
+      # it test is outside/independent of the creation to ensure it runs first
+      # it { expect(product1.quantity).not_to eql nil }
+
+      # old format
+      # it "should reject invalid product" do
+        # expect(product1.quantity).not_to eql nil
+      # end
+
+    end
+  end
+
+end
