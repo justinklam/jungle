262093ff4a2850fed7a58f588d54b731c6dfd8b0
Justin Lam <justin.kh.lam@gmail.com>
2022-01-29 04:08:55 +0000

    add password validation length min 3 chars

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true
-  validates :password_digest, presence: true
+  validates :password_digest, presence: true, length: { minimum: 3}
 
 end
