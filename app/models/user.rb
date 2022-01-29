c745d4094e27ce981cce0ffd8bbc05e89b032626
Justin Lam <justin.kh.lam@gmail.com>
2022-01-29 23:16:25 +0000

    refactor sessions controller to refer to User class method authentication there. user authentication moved to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,23 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
+  # validations
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :password_digest, presence: true, length: { minimum: 3}
+
+  # user authentication
+  def self.authenticate_with_credentials(email, password)
+    # locate user with email, remove white spaces and set all lowercase
+    user = User.find_by_email(email.strip.downcase)
+
+    if user && user.authenticate(password)
+      user
+    else
+      nil
+    end
+
+  end
 
 end
