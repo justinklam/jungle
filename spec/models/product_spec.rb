d08574d315e950b88b6d72eef967be7647b3db84
Justin Lam <justin.kh.lam@gmail.com>
2022-01-29 03:51:53 +0000

    correct logic in product_spec.rb. add validation for unique email check in user.rb, create logic for valid/invalid users in user_spec.rb

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -6,25 +6,18 @@ RSpec.describe Product, type: :model do
 
     context "Insert product" do
       it "should create a valid product" do
-        # ! return true/false if exist
         cat1 = Category.create! name: 'book'
         cat1.products.create!({ name: 'coding_book', price: 7, quantity: 3}).valid?
       end
-
-      # This should fail
-      # cat1 = Category.create! name: 'book'
-      # product1 = cat1.products.create!({ name: nil, price: 7, quantity: 3})
-      # puts product1.errors.full_messages
-      
-      # this it test is outside/independent of the creation to ensure it runs first
-      # it { expect(product1.quantity).not_to eql nil }
-
-      # old format
-      # it "should reject invalid product" do
-        # expect(product1.quantity).not_to eql nil
-      # end
-
     end
+    
+    context "Insert product" do
+      it "should not create a product" do
+        # .create! assertion is saying that it should create a valid account. so it must be removed
+        cat1 = Category.create name: 'book'
+        cat1.products.create({ name: nil, price: 7, quantity: 3}).invalid?
+      end
+    end
+ 
   end
-
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'Validations' do
+
+    context "Create User" do
+      it "should create a valid user" do
+        user1 = User.create!({first_name: 'first', last_name: 'last', email: 'name@email.com', password: '123', password_confirmation: '123' }).valid?
+      end
+    end
+
+    context "Create False User" do
+      it "should create a user that fails" do
+        user1 = User.create({first_name: 'first', last_name: 'last', email: 'name@email.com', password: '123', password_confirmation: '1234' }).invalid?
+      end
+    end
+
+  end
 end
+
+# It must be created with a password and password_confirmation fields
+# These need to match so you should have an example for where they are not the same
+# These are required when creating the model so you should also have an example for this
+# Emails must be unique (not case sensitive; for example, TEST@TEST.com should not be allowed if test@test.COM is in the database)
+# Email, first name, and last name should also be required
