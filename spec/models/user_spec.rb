0de7abb82726b1ba9d7121b6de4d6752037dbdc5
Justin Lam <justin.kh.lam@gmail.com>
2022-01-29 04:20:06 +0000

    add test logic for password length validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,21 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-
   describe 'Validations' do
-
     context "Create User" do
+
       it "should create a valid user" do
         user1 = User.create!({first_name: 'first', last_name: 'last', email: 'name@email.com', password: '123', password_confirmation: '123' }).valid?
       end
-    end
 
-    context "Create False User" do
       it "should create a user that fails" do
         user1 = User.create({first_name: 'first', last_name: 'last', email: 'name@email.com', password: '123', password_confirmation: '1234' }).invalid?
       end
-    end
 
+      it "should fail to create a user due to password length" do
+        user1 = User.create({first_name: 'first', last_name: 'last', email: 'name@email.com', password: '12', password_confirmation: '12' }).invalid?
+      end
+
+    end
   end
 end
 
